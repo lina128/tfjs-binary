b610e25a8304ebe9f80d039c4150b92678b1df01
Na Li <linazhao@google.com>
2020-04-29 18:16:40 -0700

    can process both graphModel and layersModel

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const tf = require('@tensorflow/tfjs');
 
+/**
+ *   This script is used to load a saved model and perform inference.
+ *   Run this script in console:
+ *   ./index MODEL_URL MODEL_TYPE
+ *   
+ *   MODEL_TYPE: graph_model | layers_model = graph_model
+ */
 async function main() {
     const args = process.argv.slice(2);
 
@@ -10,19 +17,42 @@ async function main() {
 
     const modelUrl = args[0];
 
-    // How to decide loadGraphModel or loadLayersModel or loadGraphModel with hub flag true.
-    const model = await tf.loadGraphModel(modelUrl);
+    const modelType = parseModelType(args[1]);
+
+    let model;
+    let result;
+
+    if (modelType === 'graph_model') {
+        model = await tf.loadGraphModel(modelUrl);
+        result = await model.executeAsync(zeros);
+    } else if (modelType === 'layers_model') {
+        model = await tf.loadLayersModel(modelUrl);
+        result = model.predict(zeros);
+    } else {
+        return 'fail';
+    }
 
     const zeros = tf.zeros([1, 224, 224, 3], 'int32');
-
-    // How to decide executeAsync or execute or predict.
-    const result = await model.executeAsync(zeros);
 
     console.log(result);
 
     zeros.dispose();
 
     return 'done';
+}
+
+function parseModelType(modelType) {
+    if (!modelType) {
+        return 'graph_model';
+    }
+
+    if ((modelType !== 'graph_model') && (modelType !== 'layers_model')) {
+        console.log(`Unhandled model type: ${modelType}. Please use a ` +
+        `valid type: graph_model, layers_model`);
+        return null;
+    }
+
+    return modelType;
 }
 
 main();
